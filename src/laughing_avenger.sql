c313248319a304627c47ad2cae3fb1fe3ee1a922
Boyang <wangboyang1991@gmail.com>
2016-05-17 06:09:25 +0800

    add drop line

--- src/laughing_avenger.sql
+++ src/laughing_avenger.sql
@@ -19,6 +19,7 @@ SET time_zone = "+00:00";
 --
 -- Database: `laughing_avenger`
 --
+DROP DATABASE IF EXISTS `laughing_avenger`;
 CREATE DATABASE IF NOT EXISTS `laughing_avenger` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `laughing_avenger`;
 
